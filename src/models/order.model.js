439f166cbb2bfb11144104bec016033048e67014
Kunal Verma <vkunal591@gmail.com>
2025-04-21 20:39:26 +0530

    Updated Order

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,16 +1,24 @@
 import BaseSchema from "#models/base";
 import { saveFile } from "#utils/uploadFile";
 import mongoose from "mongoose";
+import Product from "#models/product"
+
+const productSchema = new BaseSchema({
+  product: {
+    type: BaseSchema.Types.ObjectId,
+    required: true,
+    ref: Product
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  }
+})
 
 const orderScherma = new BaseSchema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  items: [
-    {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-      quantity: { type: Number, required: true },
-      name: { type: String }
-    },
-  ],
+  items: [productSchema],
   totalAmount: { type: Number, required: true },
   status: { type: String, default: 'Pending' },
 });
--- src/services/order.service.js
+++ src/services/order.service.js
@@ -4,6 +4,35 @@ import Service from "#services/base";
 class OrderService extends Service {
   static Model = Order;
 
+  static async get(id, filters) {
+    const initialStage = [{
+      $lookup: {
+        from: "users",
+        as: "userData",
+        localField: "userId",
+        foreignField: "_id"
+      }
+    }];
+    const extraStage = [{
+      $project: {
+        totalAmount: 1,
+        status: 1,
+        userName: { $arrayElemAt: ["$userData.name", 0] }
+      }
+    }];
+
+    if (!id) {
+      return await this.Model.findAll(filters, initialStage, extraStage);
+    }
+    return await this.Model.aggregate([{
+      $match: {
+        _id: new mongoose.Types.ObjectId(id)
+      },
+      ...initialStage, ...extraStage
+    }])
+
+  }
+
   static async getMyOrders(userId) {
     if (!userId) {
       throw new Error("User ID is required");
@@ -11,8 +40,6 @@ class OrderService extends Service {
 
     // Delete all carts associated with the user
     const result = await this.Model.findAll({ user: userId });
-
-
 
     return {
       message: `Order ${result.deletedCount} of user ${userId}`,
