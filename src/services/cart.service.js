225be082e8102e11b0bc3b897a6ee2529c347d46
Kunal Verma <vkunal591@gmail.com>
2025-05-18 23:12:44 +0530

    Added Cart Image

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -4,6 +4,94 @@ import Service from "#services/base";
 class CartService extends Service {
   static Model = Cart;
 
+  static async get(id, filters = {}) {
+    const { page = 1, limit = 10, ...restFilters } = filters;
+
+    const skip = (page - 1) * limit;
+
+    const matchStage = id
+      ? [{ $match: { _id: new mongoose.Types.ObjectId(id) } }]
+      : Object.keys(restFilters).length
+      ? [{ $match: restFilters }]
+      : [];
+
+    const pipeline = [
+      ...matchStage,
+
+      {
+        $lookup: {
+          from: "users",
+          localField: "user",
+          foreignField: "_id",
+          as: "userData",
+        },
+      },
+      { $unwind: { path: "$userData", preserveNullAndEmptyArrays: true } },
+
+      { $unwind: { path: "$items", preserveNullAndEmptyArrays: true } },
+      {
+        $lookup: {
+          from: "products",
+          localField: "items.product",
+          foreignField: "_id",
+          as: "items.productData",
+        },
+      },
+      {
+        $unwind: {
+          path: "$items.productData",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          user: { $first: "$user" },
+          userName: { $first: "$userData.name" },
+          items: {
+            $push: {
+              quantity: "$items.quantity",
+              product: "$items.productData",
+            },
+          },
+          createdAt: { $first: "$createdAt" },
+          updatedAt: { $first: "$updatedAt" },
+        },
+      },
+
+      // Pagination using facet
+      {
+        $facet: {
+          result: [
+            { $sort: { createdAt: -1 } },
+            { $skip: skip },
+            { $limit: parseInt(limit) },
+          ],
+          totalCount: [{ $count: "count" }],
+        },
+      },
+      {
+        $addFields: {
+          totalItems: { $arrayElemAt: ["$totalCount.count", 0] },
+        },
+      },
+    ];
+
+    const aggResult = await this.Model.aggregate(pipeline);
+    const { result = [], totalItems = 0 } = aggResult[0] || {};
+
+    return {
+      result,
+      pagination: {
+        totalItems,
+        totalPages: Math.ceil(totalItems / limit),
+        itemsPerPage: parseInt(limit),
+        currentPage: parseInt(page),
+      },
+    };
+  }
+
+
   static async clearCart(userId) {
     if (!userId) {
       throw new Error("User ID is required");
@@ -20,7 +108,6 @@ class CartService extends Service {
       message: `Deleted ${result.deletedCount} cart(s) for user ${userId}`,
     };
   }
-
 }
 
 export default CartService;
